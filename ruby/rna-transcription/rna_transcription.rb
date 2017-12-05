88c0760a398b4d1272ed49deca74c397d1f2ef26
monkypuzzle <paul.newsam@gmail.com>
2017-12-05 10:33:07 -0800

    Solves with case statement

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,27 @@
+module Complement
+
+  def self.of_dna(dna)
+    rna = dna.split('').map do |n|
+      # return 'A'
+      case n
+      when "A"
+        "U"
+      when "T"
+        "A"
+      when "G"
+        "C"
+      when "C"
+        "G"
+      else
+        return ""
+      end
+    end.join('')
+  end
+
+end
+
+module BookKeeping
+  VERSION = 4
+end
+
+# p Complement.of_dna('U')
