38dfb85fe9f92368968b3a4a6e783557208e73ec
monkypuzzle <paul.newsam@gmail.com>
2017-12-06 12:32:43 -0800

    Solves sieve, first pass

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,19 @@
+class Sieve
+  def initialize(n)
+    @n = n
+  end
+
+  def primes
+    return [] if @n < 2
+    return [2] if @n == 2
+    primes = [*2..@n]
+    (2..Math.sqrt(@n).floor).each do |i|
+      primes = primes - (i*2..@n).step(i).to_a
+    end
+    primes
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end
