b9070ad50126c0ef79ac0ca8e052d2bbfa7832fb
monkypuzzle <paul.newsam@gmail.com>
2017-12-11 13:29:17 -0800

    Removes extraneous lines - Sieve Ruby

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,12 +1,9 @@
 class Sieve
   def initialize(n)
     @n = n
-    @primes = [2]
   end
 
   def primes
-    return [] if @n < 2
-    return [2] if @n == 2
     (2..Math.sqrt(@n).to_i).reduce([*2..@n]) do |primes, i|
       primes - (i*2..@n).step(i).to_a
     end
