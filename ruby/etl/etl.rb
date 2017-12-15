35369fc596c70c293f8be88b279c16bdf662b904
monkypuzzle <paul.newsam@gmail.com>
2017-12-15 13:41:10 -0800

    Revises ETL in Ruby

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,10 +1,9 @@
 module ETL
   def self.transform(old)
-    new = {}
-    old.each_pair{|k,v|v.each{|x|new[x.downcase]=k}}
-    new
+    old.reduce({}){|new,(k,v)|v.each{|c|new[c.downcase]=k};new}
   end
 end
 module BookKeeping
   VERSION = 1
 end
+
