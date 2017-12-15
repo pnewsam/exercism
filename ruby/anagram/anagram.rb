beaaa6492113f05fb56b8dfe3cd6a72bf36295f3
monkypuzzle <paul.newsam@gmail.com>
2017-12-15 11:03:39 -0800

    Minor edit

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -5,8 +5,6 @@ class Anagram
   end
   def match(a)
     a.select do |x|
-      # p "#{x.chars.sort.join("").downcase} equals #{@chars}?"
-      # p "#{x.downcase} equals #{@word}?"
       x.downcase.chars.sort.join("") == @chars &&
       x.downcase != @word
     end
